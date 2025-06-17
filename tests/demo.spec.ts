b4a70b4868190d14c900f720482874497b9451dc
kstankevich <kstankevich@navicons.com>
2025-06-17 14:24:17 +0300

    Update

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -45,140 +45,140 @@ test('Create new customer', async ({ page }, testInfo) => {
   // await page.locator('iframe[title="undefined"]').contentFrame().getByRole('button', { name: 'Yes' }).click();
 });
 
-// @allure.tag: regression
-// @allure.tag: sales
-// @allure.tag: dev
-test('Open Customers and check table', async ({ page }, testInfo) => {
-  testInfo.annotations.push({ type: 'tag', description: 'regression' });
-  testInfo.annotations.push({ type: 'tag', description: 'sales' });
-  testInfo.annotations.push({ type: 'tag', description: 'dev' });
+// // @allure.tag: regression
+// // @allure.tag: sales
+// // @allure.tag: dev
+// test('Open Customers and check table', async ({ page }, testInfo) => {
+//   testInfo.annotations.push({ type: 'tag', description: 'regression' });
+//   testInfo.annotations.push({ type: 'tag', description: 'sales' });
+//   testInfo.annotations.push({ type: 'tag', description: 'dev' });
 
-  await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
-  await SearchAndOpenPage(page, 'Customers');
-  await expect(page.frameLocator('iframe[title="undefined"]').getByTitle('Show info about Customers')
-          ).toBeVisible({ timeout: 10000 });
+//   await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+//   await SearchAndOpenPage(page, 'Customers');
+//   await expect(page.frameLocator('iframe[title="undefined"]').getByTitle('Show info about Customers')
+//           ).toBeVisible({ timeout: 10000 });
 
-  await page.screenshot({ path: 'screenshots/after_open_customers_page.png' });
-});
+//   await page.screenshot({ path: 'screenshots/after_open_customers_page.png' });
+// });
 
-// @allure.tag: regression
-// @allure.tag: purchasing
-// @allure.tag: dev
-test('Open Vendors and check table', async ({ page }, testInfo) => {
-  testInfo.annotations.push({ type: 'tag', description: 'regression' });
-  testInfo.annotations.push({ type: 'tag', description: 'purchasing' });
-  testInfo.annotations.push({ type: 'tag', description: 'dev' });
+// // @allure.tag: regression
+// // @allure.tag: purchasing
+// // @allure.tag: dev
+// test('Open Vendors and check table', async ({ page }, testInfo) => {
+//   testInfo.annotations.push({ type: 'tag', description: 'regression' });
+//   testInfo.annotations.push({ type: 'tag', description: 'purchasing' });
+//   testInfo.annotations.push({ type: 'tag', description: 'dev' });
 
-  await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
-  await SearchAndOpenPage(page, 'Vendors');
-  await page.waitForTimeout(2000);
+//   await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+//   await SearchAndOpenPage(page, 'Vendors');
+//   await page.waitForTimeout(2000);
 
-  await page.screenshot({ path: 'screenshots/after_open_vendors_page.png' });
-});
+//   await page.screenshot({ path: 'screenshots/after_open_vendors_page.png' });
+// });
 
-// @allure.tag: regression
-// @allure.tag: Inventory
-// @allure.tag: dev
-test('Open Items and check table', async ({ page }, testInfo) => {
-  testInfo.annotations.push({ type: 'tag', description: 'regression' });
-  testInfo.annotations.push({ type: 'tag', description: 'Inventory' });
-  testInfo.annotations.push({ type: 'tag', description: 'dev' });
+// // @allure.tag: regression
+// // @allure.tag: Inventory
+// // @allure.tag: dev
+// test('Open Items and check table', async ({ page }, testInfo) => {
+//   testInfo.annotations.push({ type: 'tag', description: 'regression' });
+//   testInfo.annotations.push({ type: 'tag', description: 'Inventory' });
+//   testInfo.annotations.push({ type: 'tag', description: 'dev' });
 
-  await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
-  await SearchAndOpenPage(page, 'Items');
-  await expect(page.frameLocator('iframe[title="undefined"]').getByTitle('Show info about Items')
-          ).toBeVisible({ timeout: 10000 });
+//   await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+//   await SearchAndOpenPage(page, 'Items');
+//   await expect(page.frameLocator('iframe[title="undefined"]').getByTitle('Show info about Items')
+//           ).toBeVisible({ timeout: 10000 });
 
-  await page.screenshot({ path: 'screenshots/after_open_items_page.png' });
-});
+//   await page.screenshot({ path: 'screenshots/after_open_items_page.png' });
+// });
 
-// @allure.tag: regression
-// @allure.tag: sales
-// @allure.tag: dev
-test('Open Sales Orders and check table', async ({ page }, testInfo) => {
-  testInfo.annotations.push({ type: 'tag', description: 'regression' });
-  testInfo.annotations.push({ type: 'tag', description: 'sales' });
-  testInfo.annotations.push({ type: 'tag', description: 'dev' });
+// // @allure.tag: regression
+// // @allure.tag: sales
+// // @allure.tag: dev
+// test('Open Sales Orders and check table', async ({ page }, testInfo) => {
+//   testInfo.annotations.push({ type: 'tag', description: 'regression' });
+//   testInfo.annotations.push({ type: 'tag', description: 'sales' });
+//   testInfo.annotations.push({ type: 'tag', description: 'dev' });
 
-  await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
-  await SearchAndOpenPage(page, 'Sales Orders');
-  await page.waitForTimeout(2000);
+//   await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+//   await SearchAndOpenPage(page, 'Sales Orders');
+//   await page.waitForTimeout(2000);
 
-  await page.screenshot({ path: 'screenshots/after_open_sales_orders_page.png' });
-});
+//   await page.screenshot({ path: 'screenshots/after_open_sales_orders_page.png' });
+// });
 
-// @allure.tag: regression
-// @allure.tag: purchasing
-// @allure.tag: dev
-test('Open Purchase Orders and check table', async ({ page }, testInfo) => {
-  testInfo.annotations.push({ type: 'tag', description: 'regression' });
-  testInfo.annotations.push({ type: 'tag', description: 'purchasing' });
-  testInfo.annotations.push({ type: 'tag', description: 'dev' });
+// // @allure.tag: regression
+// // @allure.tag: purchasing
+// // @allure.tag: dev
+// test('Open Purchase Orders and check table', async ({ page }, testInfo) => {
+//   testInfo.annotations.push({ type: 'tag', description: 'regression' });
+//   testInfo.annotations.push({ type: 'tag', description: 'purchasing' });
+//   testInfo.annotations.push({ type: 'tag', description: 'dev' });
 
-  await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
-  await SearchAndOpenPage(page, 'Purchase Orders');
-  await page.waitForTimeout(2000);
+//   await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+//   await SearchAndOpenPage(page, 'Purchase Orders');
+//   await page.waitForTimeout(2000);
 
-  await page.screenshot({ path: 'screenshots/after_open_purchase_orders_page.png' });
-});
+//   await page.screenshot({ path: 'screenshots/after_open_purchase_orders_page.png' });
+// });
 
-// @allure.tag: regression
-// @allure.tag: finance
-// @allure.tag: dev
-test('Open Chart of Accounts and check table', async ({ page }, testInfo) => {
-  testInfo.annotations.push({ type: 'tag', description: 'regression' });
-  testInfo.annotations.push({ type: 'tag', description: 'finance' });
-  testInfo.annotations.push({ type: 'tag', description: 'dev' });
+// // @allure.tag: regression
+// // @allure.tag: finance
+// // @allure.tag: dev
+// test('Open Chart of Accounts and check table', async ({ page }, testInfo) => {
+//   testInfo.annotations.push({ type: 'tag', description: 'regression' });
+//   testInfo.annotations.push({ type: 'tag', description: 'finance' });
+//   testInfo.annotations.push({ type: 'tag', description: 'dev' });
 
-  await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
-  await page.waitForTimeout(2000);
+//   await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+//   await page.waitForTimeout(2000);
 
-  await page.screenshot({ path: 'screenshots/after_open_chart_of_accounts_page.png' });
-});
+//   await page.screenshot({ path: 'screenshots/after_open_chart_of_accounts_page.png' });
+// });
 
-// @allure.tag: regression
-// @allure.tag: sales
-// @allure.tag: dev
-test('Open Posted Sales Invoices and check table', async ({ page }, testInfo) => {
-  testInfo.annotations.push({ type: 'tag', description: 'regression' });
-  testInfo.annotations.push({ type: 'tag', description: 'sales' });
-  testInfo.annotations.push({ type: 'tag', description: 'dev' });
+// // @allure.tag: regression
+// // @allure.tag: sales
+// // @allure.tag: dev
+// test('Open Posted Sales Invoices and check table', async ({ page }, testInfo) => {
+//   testInfo.annotations.push({ type: 'tag', description: 'regression' });
+//   testInfo.annotations.push({ type: 'tag', description: 'sales' });
+//   testInfo.annotations.push({ type: 'tag', description: 'dev' });
 
-  await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
-  await SearchAndOpenPage(page, 'Posted Sales Invoices');
-  await page.waitForTimeout(2000);
+//   await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+//   await SearchAndOpenPage(page, 'Posted Sales Invoices');
+//   await page.waitForTimeout(2000);
 
-  await page.screenshot({ path: 'screenshots/after_open_posted_sales_invoices_page.png' });
-});
+//   await page.screenshot({ path: 'screenshots/after_open_posted_sales_invoices_page.png' });
+// });
 
-// @allure.tag: regression
-// @allure.tag: purchasing
-// @allure.tag: dev
-test('Open Posted Purchase Invoices and check table', async ({ page }, testInfo) => {
-  testInfo.annotations.push({ type: 'tag', description: 'regression' });
-  testInfo.annotations.push({ type: 'tag', description: 'purchasing' });
-  testInfo.annotations.push({ type: 'tag', description: 'dev' });
+// // @allure.tag: regression
+// // @allure.tag: purchasing
+// // @allure.tag: dev
+// test('Open Posted Purchase Invoices and check table', async ({ page }, testInfo) => {
+//   testInfo.annotations.push({ type: 'tag', description: 'regression' });
+//   testInfo.annotations.push({ type: 'tag', description: 'purchasing' });
+//   testInfo.annotations.push({ type: 'tag', description: 'dev' });
 
-  await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
-  await SearchAndOpenPage(page, 'Posted Purchase Invoices');
-  await page.waitForTimeout(2000);
+//   await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+//   await SearchAndOpenPage(page, 'Posted Purchase Invoices');
+//   await page.waitForTimeout(2000);
 
-  await page.screenshot({ path: 'screenshots/after_open_posted_purchase_invoices_page.png' });
-});
+//   await page.screenshot({ path: 'screenshots/after_open_posted_purchase_invoices_page.png' });
+// });
 
-// @allure.tag: regression
-// @allure.tag: finance
-// @allure.tag: dev
-test('Open Bank Accounts and check table', async ({ page }, testInfo) => {
-  testInfo.annotations.push({ type: 'tag', description: 'regression' });
-  testInfo.annotations.push({ type: 'tag', description: 'finance' });
-  testInfo.annotations.push({ type: 'tag', description: 'dev' });
+// // @allure.tag: regression
+// // @allure.tag: finance
+// // @allure.tag: dev
+// test('Open Bank Accounts and check table', async ({ page }, testInfo) => {
+//   testInfo.annotations.push({ type: 'tag', description: 'regression' });
+//   testInfo.annotations.push({ type: 'tag', description: 'finance' });
+//   testInfo.annotations.push({ type: 'tag', description: 'dev' });
 
-  await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
-  await SearchAndOpenPage(page, 'Bank Accounts');
-  await expect(page.frameLocator('iframe[title="undefined"]').getByTitle('Show info about Bank Accounts')
-          ).toBeVisible({ timeout: 10000 });
+//   await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+//   await SearchAndOpenPage(page, 'Bank Accounts');
+//   await expect(page.frameLocator('iframe[title="undefined"]').getByTitle('Show info about Bank Accounts')
+//           ).toBeVisible({ timeout: 10000 });
 
-  await page.screenshot({ path: 'screenshots/after_open_bank_accounts_page.png' });
-});
+//   await page.screenshot({ path: 'screenshots/after_open_bank_accounts_page.png' });
+// });
 
