7520bcd13f1a8000f40f4fda420a5166c84931b0
kstankevich <kstankevich@navicons.com>
2025-07-09 13:02:58 +0300

    Update090725

--- tests/bridge_functions.js
+++ tests/bridge_functions.js
@@ -2,7 +2,7 @@ import { randomInt } from "crypto";
 import { chromium } from 'playwright';
 
 export async function SearchAndOpenPage(page, searchText) {
-    await page.goto('http://nav-buscent24w2:8080/BC252/?company=CRONUS%20AG');
+    await page.goto('');
     await page.getByLabel('Search').click();
     await page.frameLocator('iframe[title="undefined"]').getByRole('textbox', { name: 'Tell me what you want to do' }).fill(searchText);
     await page.frameLocator('iframe[title="undefined"]').locator('[id^="GroupedListSection"]').getByText(searchText, { exact: true }).click();
@@ -15,8 +15,7 @@ export function RandInt(range) {
   }
 
 export async function takeScreenshotAndAttach(page, testInfo, name, path) {
-    await page.screenshot({ path });
-    await testInfo.attach(name, { path, contentType: 'image/png' });
+  await page.waitForTimeout(4000);  
+  await page.screenshot({ path });
+  await testInfo.attach(name, { path, contentType: 'image/png' });
 }
-
-  
